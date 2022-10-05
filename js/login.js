ecefd593761417bd9c54d235f3fef700ef68a2ec
kenzach07 <charlsongeronimo@gmail.com>
2022-10-05 13:04:09 +0800

    new updates

--- js/login.js
+++ js/login.js
@@ -3,28 +3,18 @@
     var email = document.forms['form']['email'];
     var password = document.forms['form']['password'];
     
-    var email_error = document.getElementById('email_error');
-    var pass_error = document.getElementById('pass_error');
-    
-    email.addEventListener('textInput', email_Verify);
-    password.addEventListener('textInput', pass_Verify);
-    
     function validated(){
         if (email.value != "kenzach" || password.value != "cool") {
             email.value="";
             password.value = "";
             alert("WRONG CREDENTIALS");
             return false;
+        }else{
+            return true;
         }
     
     }
-    function email_Verify(){
-        if (email.value == "kenzach" || password.value == "cool") {
-            email.style.border = "1px solid silver";
-            email_error.style.display = "none";
-            return true;
-        }
-    }
+   
   
 
    
--- js/script.js
+++ js/script.js
@@ -1,117 +1,164 @@
 var selectedRow = null;
 var id = 3;
+var db = openDatabase("itemDB", "1.0", "itemDB", 65535);
 
+//show updated table data at load
+window.onload = function () {
+  document.getElementById("list").click();
+};
 
 //show alert
-function showAlert(message, className){
-    const div = document.createElement("div");
-    div.className = `alert alert-${className}`;
+function showAlert(message, className) {
+  const div = document.createElement("div");
+  div.className = `alert alert-${className}`;
 
-    div.appendChild(document.createTextNode(message));
-    const container = document.querySelector(".container");
-    const main = document.querySelector(".main");
-    container.insertBefore(div, main);
+  div.appendChild(document.createTextNode(message));
+  const container = document.querySelector(".container");
+  const main = document.querySelector(".main");
+  container.insertBefore(div, main);
 
-    setTimeout(() => document.querySelector(".alert").remove(), 3000);
+  setTimeout(() => document.querySelector(".alert").remove(), 3000);
 }
 
-
-
-
 //clear all fields
-function clearFields(){
-    document.querySelector("#projectName").value = "";
-    document.querySelector("#description").value = "";
-    document.querySelector("#targetID").value = "";
-    document.querySelector("#completedID").value = "";
+function clearFields() {
+  document.querySelector("#projectName").value = "";
+  document.querySelector("#description").value = "";
+  document.querySelector("#targetID").value = "";
+  document.querySelector("#completedID").value = "";
 }
 
 //clear button function
 document.querySelector("#career-form").addEventListener("reset", (e) => {
-    clearFields();
+  clearFields();
 });
 
-//add data
-document.querySelector("#career-form").addEventListener("submit", (e) => {
-    e.preventDefault();
+//CREATE A TABLE
+document.querySelector("#career-form").addEventListener("click", (e) => {
+  target = e.target;
+  if (target.classList.contains("create")) {
+    db.transaction(function (transaction) {
+      var sql = "CREATE TABLE items " + "(id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT," + "projectname VARCHAR(30) NOT NULL," + "descrip VARCHAR(30) NOT NULL," + "targetDate DATE NOT NULL, " + "completeDate DATE)";
+      transaction.executeSql(
+        sql,
+        undefined,
+        function () {
+          alert("Table is created successfully");
+        },
+        function () {
+          alert("Table not created");
+        }
+      );
+    });
+  }
 
-    const projectName = document.querySelector("#projectName").value;
-    const description = document.querySelector("#description").value;
-    const targetID = document.querySelector("#targetID").value;
-    const completedID = document.querySelector("#completedID").value;
+  //INSERT A TABLE IN DATA
+  if (target.classList.contains("insert")) {
+    const projectNameA = document.querySelector("#projectName").value;
+    const descriptionA = document.querySelector("#description").value;
+    const targetIDA = document.querySelector("#targetID").value;
+    const completedIDA = document.querySelector("#completedID").value;
 
-    //validation if empty field
-    if(projectName =="" || description =="" || targetID=="" || completedID==""){
-    showAlert("Please fill in all fields", "danger");
-    }else{
-    if(selectedRow == null){
-        switcher = true;
-        const list = document.querySelector("#project-list");
+    if (projectName === "" || description === "" || targetID === "" || completedID === "") {
+      showAlert("Please fill in all fields", "danger");
+    } else {
+      if (selectedRow === null) {
+        db.transaction(function (transaction) {
+          var sql = "INSERT INTO items(projectname, descrip, targetDate, completeDate) VALUES(?,?,?,?)";
+          transaction.executeSql(
+            sql,
+            [projectNameA, descriptionA, targetIDA, completedIDA],
+            function () {
+              selectedRow = null;
+              showAlert("NEW ENTRY ADDED", "success");
 
-        const row = document.createElement("tr");
-
-        row.innerHTML = `
-            <td>${id++}</td>
-            <td>${projectName}</td>
-            <td>${description}</td>
-            <td>${targetID}</td>
-            <td>${completedID}</td>
-            <td>
-            <a href="#" class="btn btn-warning btn-sm edit"> Edit </a>
-            <a href="#" class="btn btn-danger btn-sm delete"> Delete </a>
-            </td>
-        `;
-        list.appendChild(row);
-        selectedRow = null;
-        showAlert("NEW ENTRY ADDED", "success");
-    }
-    else{
-        
-        
-        selectedRow.children[1].textContent = projectName;
-        selectedRow.children[2].textContent = description;
-        selectedRow.children[3].textContent = targetID;
-        selectedRow.children[4].textContent = completedID;
+              clearFields();
+              loadData();
+            },
+            function (transaction, err) {
+              alert(err.message);
+            }
+          );
+        });
+      } else {
+        selectedRow.children[1].textContent = projectNameA;
+        selectedRow.children[2].textContent = descriptionA;
+        selectedRow.children[3].textContent = targetIDA;
+        selectedRow.children[4].textContent = completedIDA;
         selectedRow = null;
         showAlert("ENTRY UPDATED", "info");
-        
+      }
+      clearFields();
     }
-    clearFields();
-}
+  }
+
+  //FOR LOADING THE DATA
+  if (target.classList.contains("list")) {
+    loadData();
+  }
+
+  function loadData() {
+    $("#project-list").children().remove();
+    db.transaction(function (transaction) {
+      var sql = "SELECT * FROM items ORDER BY id ASC";
+      transaction.executeSql(
+        sql,
+        undefined,
+        function (transaction, result) {
+          if (result.rows.length) {
+            for (var i = 0; i < result.rows.length; i++) {
+              var row = result.rows.item(i);
+              var projectname = row.projectname;
+              var id = row.id;
+              var descrip = row.descrip;
+              var tDate = row.targetDate;
+              var cDate = row.completeDate;
+              $("#project-list").append('<tr id="del' + id + '"><td id=' + id + ">" + id + "</td><td>" + projectname + "</td><td>" + descrip + "</td><td>" + tDate + "</td><td>" + cDate + '</td><td><a href="#" class="btn btn-primary edit" data-id="' + id + '">Edit</a>  <a href="#" class="btn btn-danger delete" data-id="' + id + '">Delete</a></td></tr>');
+            }
+          } else {
+            $("#project-list").append('<tr><td colspan="3" align="center">No Item Found</td></tr>');
+          }
+        },
+        function (transaction, err) {
+          alert('No table found. Click on "Create Table" to create table now');
+        }
+      );
+    });
+  }
 });
 
 //edit data
 document.querySelector("#project-list").addEventListener("click", (e) => {
-    target = e.target;
+  target = e.target;
+  if (target.classList.contains("edit")) {
+    selectedRow = target.parentElement.parentElement;
 
-    if(target.classList.contains("edit")){
-        selectedRow = target.parentElement.parentElement;
-        
-        document.querySelector("#projectName").value = selectedRow.children[1].textContent; 
-        document.querySelector("#description").value = selectedRow.children[2].textContent; 
-        document.querySelector("#targetID").value = selectedRow.children[3].textContent; 
-        document.querySelector("#completedID").value = selectedRow.children[4].textContent; 
-    }
+    document.querySelector("#projectName").value = selectedRow.children[1].textContent;
+    document.querySelector("#description").value = selectedRow.children[2].textContent;
+    document.querySelector("#targetID").value = selectedRow.children[3].textContent;
+    document.querySelector("#completedID").value = selectedRow.children[4].textContent;
+  }
 
-});
-
-
-//delete data
-document.querySelector("#project-list").addEventListener("click", (e) => {
-    
-    target = e.target;
-    
-    if(target.classList.contains("delete")){
-        if (confirm("ARE YOU SURE YOU WANT TO DELETE?") == true) {
-            target.parentElement.parentElement.remove();
-            clearFields();
-            showAlert("CAREER DATA DELETED", "info");
-          } else {
-            showAlert("CAREER DATA NOT DELETED", "danger");
+  //delete data
+  if (target.classList.contains("delete")) {
+    selectedRow = target.parentElement.parentElement;
+    var sure = confirm("Are you sure to delete this item?");
+    if (sure === true) {
+      var id = selectedRow.children[0].textContent;
+      db.transaction(function (transaction) {
+        var sql = "DELETE FROM items where id=?";
+        transaction.executeSql(
+          sql,
+          [id],
+          function () {
+            selectedRow.remove();
+            showAlert("ENTRY DELETED", "danger");
+          },
+          function (transaction, err) {
+            alert(err.message);
           }
-        
-        
-    }   
-
-    
+        );
+      });
+    }
+  }
 });
